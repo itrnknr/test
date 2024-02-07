9350f349ef1d0fd328c178ea960e8a8d9403efd4
Yaroslav <itrnknr@gmail.com>
2024-02-07 19:48:38 +0300

    new features added

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+from collections import namedtuple
+
 import telebot
 import requests
 
@@ -10,15 +13,43 @@ bot = telebot.TeleBot(token)
 def greet_humam(message):
     bot.send_message(message.chat.id, "Hello!")
 
+
+coordinates = namedtuple('Coordinates', ('latitude', 'longitude'))
+cities = {
+    'Yerevan': coordinates(40.178, 40.505),
+
+}
+
+users = {}
 @bot.message_handler(commands=['get_weather'])
 def get_weather(message):
-    url = f'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={59.937}&lon={30.308}&appid={weather_api_key}'
-    response = requests.get(url)
-    status = response.status_code
-    if status==200:
-        weather = response.json()
-        bot.send_message(message.chat.id, weather["weather"][0]["description"])
+    user_city = users.get(message.chat.id)
+
+    if user_city and user_city in cities:
+        coordinates = cities[user_city]
+        url = f'https://api.openweathermap.org/data/2.5/weather?lat={coordinates.latitude}&lon={coordinates.longitude}&appid={weather_api_key}'
+        response = requests.get(url)
+        status = response.status_code
+        if status==200:
+            weather = response.json()
+            bot.send_message(message.chat.id, weather["weather"][0]["description"])
+        else:
+            bot.send_message(message.chat.id, "weather is not available")
     else:
-        bot.send_message(message.chat.id, "weather is not available")
+        bot.send_message(message.chat.id, "ERROR")
+
+
+@bot.message_handler(commands=['setup'])
+def ask_city(message):
+    bot.send_message(message.chat.id, "Каков ваш город?")
+    bot.register_next_step_handler(message, proceed_city)
+
+def proceed_city(message):
+    bot.send_message(message.chat.id, f'крутой город - {message.text}!')
+    users[message.chat.id] = message.text
+@bot.message_handler(commands=['send_picture'])
+def send_picture(message):
+    url = f'https://photostorage.explorest.com/usa/washington/mmatti-mount-shuksan-picture-compressed.jpg'
+    bot.send_photo(message.chat_id, url)
 
 bot.polling()
